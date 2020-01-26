4af7e3cfbe6b9573d0ce4273983dd9a9cdd439a4
Klinikowski <klinikowskikrystian@gmail.com>
2020-01-26 19:53:33 +0100

    Add Games,Aktor,Theater Arts to routes

--- api/models/Theater_Arts.js
+++ api/models/Theater_Arts.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const theater_artsSchema = mongoose.Schema({
+const Theater_ArtsSchema = mongoose.Schema({
     _id: mongoose.Types.ObjectId,
     title: String,
     rating: Number,
     description: String,
     category: String,
     location: String,
-    premiera: new Date(year, month, day)
+    premiere: new Date(year, month, day)
 });
 
-module.exports = mongoose.model("theater arts", theater_artsSchema);
+module.exports = mongoose.model("theater arts", Theater_ArtsSchema);
